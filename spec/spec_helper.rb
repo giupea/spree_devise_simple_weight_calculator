012202b0f286a9972ab5b42fcb0127f0aff65f34
Massimo Mori <massimo.mori@devise.it>
2013-11-29 12:54:17 +0100

    name changes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -28,8 +28,8 @@ require 'spree/testing_support/controller_requests'
 require 'spree/testing_support/authorization_helpers'
 require 'spree/testing_support/url_helpers'
 
-# Requires factories defined in lib/spree_simple_weight_calculator/factories.rb
-require 'spree_simple_weight_calculator/factories'
+# Requires factories defined in lib/spree_devise_simple_weight_calculator/factories.rb
+require 'spree_devise_simple_weight_calculator/factories'
 
 RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
